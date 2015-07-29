1ad2eccc58a424222e8a4f7825b7b9c6f2807ca9
ahamouda88 <ahmed.hamouda8888@gmail.com>
2015-07-29 14:31:54 -0400

    Change HTTP status

--- src/main/java/com/queue/exception/IdAlreadyExistsException.java
+++ src/main/java/com/queue/exception/IdAlreadyExistsException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class IdAlreadyExistsException extends RuntimeException{
 	
 	private static final long serialVersionUID = 6313903570316610980L;
