b978afa40229a00a2ad88c332ae7a1d3bbe53cc9
ahamouda88 <ahmed.hamouda8888@gmail.com>
2015-07-29 12:53:41 -0400

    Fix Spring config

--- src/main/java/com/queue/spring/SpringConfiguration.java
+++ src/main/java/com/queue/spring/SpringConfiguration.java
@@ -2,16 +2,23 @@ package com.queue.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages={"com.queue.impl", "com.queue.service", "com.queue.rest"})
 @EnableAutoConfiguration
-public class SpringConfiguration {
+public class SpringConfiguration extends SpringBootServletInitializer{
 
 	 public static void main(String[] args) {
 	        SpringApplication.run(SpringConfiguration.class, args);
 	 }
+	 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringConfiguration.class);
+	}
 	 
 }
